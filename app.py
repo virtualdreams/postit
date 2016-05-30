8ccd62880fbbd2b244b56a510711a01c0e42c972
Thomas Kindler <virtualdreamsdd@aol.com>
2016-05-30 15:44:32 +0200

    Moved most code to class and add more sanitizing.

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ import re
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from flask import Flask, json, request, render_template, abort, Response
+from database import PostitDb
 
 app = Flask(__name__)
 app.config.update(
 	PROPAGATE_EXCEPTIONS = True
 )
 
-
+# postit database
 postitCollection = MongoClient().postit.postits
+
+db = PostitDb()
 
 # Test for empty string
 IsNullOrEmpty = lambda s: True if s and s.strip() else False
@@ -23,67 +26,23 @@ def page_not_found(e):
 
 @app.route('/')
 def home():
-	view = []
-	for postit in postitCollection.find().sort('_id', -1):
-		id = str(postit['_id'])
-		title = postit['title']
-		content = postit['content']
-		content = content.replace('\n', '<br />')
-		content = re.sub(r'#([a-zA-Z0-9]+)', r'<a href="/h/\1">#\1</a>', content)
-		content = re.sub(r'(((https?|ftp)://|www.)[^\s<]+[^\s<\.)])', r'<a href="\1">\1</a>', content)
-		post = {
-			'title': title,
-			'content': content,
-			'id': id
-		}
-		view.append(post)
+	_result = db.getAll()
 	
-	return render_template('_postit.html', view = view)
+	return render_template('_postit.html', view = _result)
 
 @app.route('/h/<hash>')
 def get_hash(hash = None):
-	view = []
-	for postit in postitCollection.find({ 'tags' : { '$elemMatch' : { '$regex' : hash, '$options' : 'i' }}}).sort('_id', -1):
-		id = str(postit['_id'])
-		title = postit['title']
-		content = postit['content']
-		content = content.replace('\n', '<br />')
-		content = re.sub(r'#([a-zA-Z0-9]+)', r'<a href="/h/\1">#\1</a>', content)
-		content = re.sub(r'(((https?|ftp)://|www.)[^\s<]+[^\s<\.)])', r'<a href="\1">\1</a>', content)
-		post = {
-			'id': id,
-			'title': title,
-			'content': content
-		}
-		view.append(post)
-	
-	return render_template('_postit.html', view = view)
+	_result = db.getByHash(hash)
+
+	return render_template('_postit.html', view = _result)
 
 @app.route('/s/<id>')
 def get_submit(id = None):
-	_id = ()
-	try:
-		_id = ObjectId(id)
-	except:
+	_result = db.getById(id)
+	if not _result:
 		abort(404)
 	
-	postit = postitCollection.find_one({'_id': _id})
-	if postit is None:
-		abort(404)
-		
-	id = str(postit['_id'])
-	title = postit['title']
-	content = postit['content']
-	content = content.replace('\n', '<br />')
-	content = re.sub(r'#([a-zA-Z0-9]+)', r'<a href="/h/\1">#\1</a>', content)
-	content = re.sub(r'(((https?|ftp)://|www.)[^\s<]+[^\s<\.)])', r'<a href="\1">\1</a>', content)
-	view = {
-		'id': id,
-		'title': title,
-		'content': content
-	}
-	
-	return render_template('_one.html', view = view)
+	return render_template('_one.html', view = _result)
 	
 @app.route('/submit', methods = ['GET'])
 def create_submit():
@@ -93,90 +52,43 @@ def create_submit():
 def post_submit():
 	r = request.json
 	if r is not None:
-		#title = re.sub('<[^<]+?>', '', r['title'])
-		#content = re.sub('<[^<]+?>', '', r['content'])
-		title = r['title'].replace('<', '&lt;').replace('>', '&gt;')
-		content = r['content'].replace('<', '&lt;').replace('>', '&gt;')
-		if IsNullOrEmpty(title) is not False:
-			tags = re.findall(r'#([a-zA-Z0-9]+)', r['content'])
-			postit = {
-				'title': title,
-				'content': content,
-				'tags': tags
-			}
-			postitCollection.insert(postit)
-	
+		_result = db.add(r.get('title'), r.get('content'))
+		if _result is not None:
 			return json.dumps({})
-	
-	return json.dumps({}), 404
+			
+	return json.dumps({}), 500
 	
 @app.route('/edit/<id>', methods = ['GET'])
 def get_edit(id	= None):
-	_id = ()
-	try:
-		_id = ObjectId(id)
-	except:
+	_result = db.getById(id, True)
+	if not _result:
 		abort(404)
 	
-	postit = postitCollection.find_one({'_id': _id})
-	if postit is None:
-		abort(404)
-	
-	view = {
-		'id': str(postit['_id']),
-		'title': postit['title'],
-		'content': postit['content']
-	}
-	
-	return render_template('_edit.html', view = view)
+	return render_template('_edit.html', view = _result)
 	
 @app.route('/edit/<id>', methods = ['POST'])
 def post_edit(id = None):
-	_id = ()
-	try:
-		_id = ObjectId(id)
-	except:
-		abort(404)
-	
 	r = request.json
 	if r is not None:
-		#title = re.sub('<[^<]+?>', '', r['title'])
-		#content = re.sub('<[^<]+?>', '', r['content'])
-		title = r['title'].replace('<', '&lt;').replace('>', '&gt;')
-		content = r['content'].replace('<', '&lt;').replace('>', '&gt;')
-		if IsNullOrEmpty(title) is not False:
-			tags = re.findall(r'#([a-zA-Z0-9]+)', r['content'])
-			postit = {
-				'title': title,
-				'content': content,
-				'tags': tags
-			}
-			postitCollection.update({ '_id': _id }, { '$set': postit })
-			
+		_result = db.update(id, r.get('title'), r.get('content'))
+		if _result:
 			return json.dumps({})
-			
-	return json.dumps({}), 404
-
-@app.route('/api/s/list')
-def api_list_submits():
-	view = []
-	for postit in postitCollection.find().sort('_id', 0):
-		post = {
-			'id': str(postit['_id']),
-			'title': postit['title'],
-			'content': postit['content']
-		}
-		view.append(post)
-	
-	return Response(json.dumps(view), mimetype='application/json')
-	
-def prepare():
-	title = r['title'].replace('<', '&lt;').replace('>', '&gt;')
-	content = r['content'].replace('<', '&lt;').replace('>', '&gt;')
-	content = content.replace('\n', '<br />')
-	content = re.sub(r'#([a-zA-Z0-9]+)', r'<a href="/h/\1">#\1</a>', content)
-	content = re.sub(r'(((https?|ftp)://|www.)[^\s<]+[^\s<\.)])', r'<a href="\1">\1</a>', content)
+		
+	return json.dumps({}), 500
+		
+#@app.route('/api/s/list')
+#def api_list_submits():
+#	view = []
+#	for postit in postitCollection.find().sort('_id', 0):
+#		post = {
+#			'id': str(postit['_id']),
+#			'title': postit['title'],
+#			'content': postit['content']
+#		}
+#		view.append(post)
+#	
+#	return Response(json.dumps(view), mimetype='application/json')
 		
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0', port = 8080, debug = True)
+	app.run(host = '0.0.0.0', port = 8081, debug = True)
 
---
+++ database.py
@@ -0,0 +1,118 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import re
+
+class PostitDb():
+	def __init__(self):
+		self.collection = MongoClient().postit.postits
+		
+	def add(self, title, content):
+		if title and content:
+			post = {
+				'title': title,
+				'content': content,
+				'tags': re.findall(r'#([a-zA-Z0-9]+)', content)
+			}
+			self.collection.insert(post)
+			return post.get('_id')
+		else:
+			return None
+		
+	def update(self, id, title, content):
+		_id = ()
+		try:
+			_id = ObjectId(id)
+		except:
+			return False
+		
+		if title and content:
+			post = {
+				'title': title,
+				'content': content,
+				'tags': re.findall(r'#([a-zA-Z0-9]+)', content)
+			}
+			self.collection.update({'_id': _id}, { '$set': post })
+			return True
+		
+		return False
+		
+	def getAll(self):
+		for postit in self.collection.find().sort('_id', -1):##.skip(5).limit(5):
+			# get the values
+			_title = postit.get('title')
+			_content = postit.get('content')
+			
+			# sanitizing
+			_title = self._sanitize(_title)
+			_content = self._sanitize(_content)
+			
+			# replace links and hashes
+			_content = self._prepare(_content)
+			
+			# create the object
+			post = {
+				'id': str(postit.get('_id')),
+				'title': _title,
+				'content': _content
+			}
+			yield post
+		
+	def getById(self, id, raw = False):
+		_id = ()
+		try:
+			_id = ObjectId(id)
+		except:
+			return None
+			
+		postit = self.collection.find_one({'_id': _id})
+		if not postit:
+			return None
+		
+		_title = postit.get('title')
+		_content = postit.get('content')
+		
+		if not raw:
+			# sanitizing
+			_title = self._sanitize(_title)
+			_content = self._sanitize(_content)
+			
+			# replace links and hashes
+			_content = self._prepare(_content)
+		
+		# create the object
+		post = {
+			'id': str(postit.get('_id')),
+			'title': _title,
+			'content': _content
+		}
+		return post
+		
+	def getByHash(self, hash):
+		for postit in self.collection.find({ 'tags' : { '$elemMatch' : { '$regex' : hash, '$options' : 'i' }}}).sort('_id', -1):
+			# get the values
+			_title = postit.get('title')
+			_content = postit.get('content')
+			
+			# sanitizing
+			_title = self._sanitize(_title)
+			_content = self._sanitize(_content)
+			
+			# replace links and hashes
+			_content = self._prepare(_content)
+			
+			# create the object
+			post = {
+				'id': str(postit.get('_id')),
+				'title': _title,
+				'content': _content
+			}
+			yield post
+		
+	def _sanitize(self, value):
+		return value.replace('<', '&lt;').replace('>', '&gt;').replace('\n', '<br />')
+		
+	def _prepare(self, value):
+		_value = value
+		_value = re.sub(r'#([a-zA-Z0-9]+)', r'<a href="/h/\1">#\1</a>', _value)
+		_value = re.sub(r'(((https?|ftp)://|www.)[^\s<]+[^\s<\.)])', r'<a href="\1">\1</a>', _value)
+		return _value
