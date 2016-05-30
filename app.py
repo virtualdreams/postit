8a32066a67b84caca4a3270693ec1eade6aed115
Thomas Kindler <virtualdreamsdd@aol.com>
2016-05-30 15:44:24 +0200

    Added editing and 404 page.

--- app.py
+++ app.py
@@ -3,55 +3,169 @@
 import pymongo
 import re
 from pymongo import MongoClient
-from flask import Flask, json, request, render_template
-from jinja2 import Markup
+from bson.objectid import ObjectId
+from flask import Flask, json, request, render_template, abort, Response
 
 app = Flask(__name__)
 
 postitCollection = MongoClient().postit.postits
 
+# Test for empty string
+IsNullOrEmpty = lambda s: True if s and s.strip() else False
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('_404.html'), 404
+
 @app.route('/')
 def home():
-	postits = []
+	view = []
 	for postit in postitCollection.find().sort('_id', 0):
+		id = str(postit['_id'])
+		title = postit['title']
 		content = postit['content']
 		content = content.replace('\n', '<br />')
 		content = re.sub(r'#([a-zA-Z0-9]+)', r'<a href="/h/\1">#\1</a>', content)
 		content = re.sub(r'(((https?|ftp)://|www.)[^\s<]+[^\s<\.)])', r'<a href="\1">\1</a>', content)
-		postit['content'] = content
-		postits.append(postit)
-
-	return render_template('_postit.html', postits = postits)
+		post = {
+			'title': title,
+			'content': content,
+			'id': id
+		}
+		view.append(post)
+	
+	return render_template('_postit.html', view = view)
 
 @app.route('/h/<hash>')
-def hash(hash = None):
-	postits = []
+def get_hash(hash = None):
+	view = []
 	for postit in postitCollection.find({ 'tags' : { '$elemMatch' : { '$regex' : hash, '$options' : 'i' }}}).sort('_id', 0):
+		id = str(postit['_id'])
+		title = postit['title']
 		content = postit['content']
+		content = content.replace('\n', '<br />')
 		content = re.sub(r'#([a-zA-Z0-9]+)', r'<a href="/h/\1">#\1</a>', content)
 		content = re.sub(r'(((https?|ftp)://|www.)[^\s<]+[^\s<\.)])', r'<a href="\1">\1</a>', content)
-		postit['content'] = content
-		postits.append(postit)
+		post = {
+			'id': id,
+			'title': title,
+			'content': content
+		}
+		view.append(post)
 	
-	return render_template('_postit.html', postits = postits)
+	return render_template('_postit.html', view = view)
+
+@app.route('/s/<id>')
+def get_submit(id = None):
+	_id = ()
+	try:
+		_id = ObjectId(id)
+	except e:
+		abort(404)
+	
+	postit = postitCollection.find_one({'_id': _id})
+	if postit is None:
+		abort(404)
+		
+	id = str(postit['_id'])
+	title = postit['title']
+	content = postit['content']
+	content = content.replace('\n', '<br />')
+	content = re.sub(r'#([a-zA-Z0-9]+)', r'<a href="/h/\1">#\1</a>', content)
+	content = re.sub(r'(((https?|ftp)://|www.)[^\s<]+[^\s<\.)])', r'<a href="\1">\1</a>', content)
+	view = {
+		'id': id,
+		'title': title,
+		'content': content
+	}
+	
+	return render_template('_one.html', view = view)
 	
 @app.route('/submit', methods = ['GET'])
-def submit():
+def create_submit():
 	return render_template('_submit.html')
 	
 @app.route('/submit', methods = ['POST'])
-def submit_post():
+def post_submit():
 	r = request.json
 	if r is not None:
-		tags = re.findall(r'#([a-zA-Z0-9]+)', r['content'])
-		postit = {
-			'title': r['title'],
-			'content': r['content'],
-			'tags': tags
+		#title = re.sub('<[^<]+?>', '', r['title'])
+		#content = re.sub('<[^<]+?>', '', r['content'])
+		title = r['title'].replace('<', '&lt;').replace('>', '&gt;')
+		content = r['content'].replace('<', '&lt;').replace('>', '&gt;')
+		if IsNullOrEmpty(title) is not False:
+			tags = re.findall(r'#([a-zA-Z0-9]+)', r['content'])
+			postit = {
+				'title': title,
+				'content': content,
+				'tags': tags
+			}
+			postitCollection.insert(postit)
+	
+			return json.dumps({})
+	
+	return json.dumps({}), 404
+	
+@app.route('/edit/<id>', methods = ['GET'])
+def get_edit(id	= None):
+	_id = ()
+	try:
+		_id = ObjectId(id)
+	except e:
+		abort(404)
+	
+	postit = postitCollection.find_one({'_id': _id})
+	if postit is None:
+		abort(404)
+	
+	view = {
+		'id': str(postit['_id']),
+		'title': postit['title'],
+		'content': postit['content']
+	}
+	
+	return render_template('_edit.html', view = view)
+	
+@app.route('/edit/<id>', methods = ['POST'])
+def post_edit(id = None):
+	_id = ()
+	try:
+		_id = ObjectId(id)
+	except e:
+		abort(404)
+	
+	r = request.json
+	if r is not None:
+		#title = re.sub('<[^<]+?>', '', r['title'])
+		#content = re.sub('<[^<]+?>', '', r['content'])
+		title = r['title'].replace('<', '&lt;').replace('>', '&gt;')
+		content = r['content'].replace('<', '&lt;').replace('>', '&gt;')
+		if IsNullOrEmpty(title) is not False:
+			tags = re.findall(r'#([a-zA-Z0-9]+)', r['content'])
+			postit = {
+				'title': title,
+				'content': content,
+				'tags': tags
+			}
+			postitCollection.update({ '_id': _id }, { '$set': postit })
+			
+			return json.dumps({})
+			
+	return json.dumps({}), 404
+
+@app.route('/api/s/list')
+def api_list_submits():
+	view = []
+	for postit in postitCollection.find().sort('_id', 0):
+		post = {
+			'id': str(postit['_id']),
+			'title': postit['title'],
+			'content': postit['content']
 		}
-		postitCollection.insert(postit)
+		view.append(post)
 	
-	return json.dumps({})
-	
-app.run(host = '0.0.0.0', port = 8080, debug = True)
+	return Response(json.dumps(view), mimetype='application/json')
+		
+if __name__ == '__main__':
+	app.run(host = '0.0.0.0', port = 8080, debug = True)
 
